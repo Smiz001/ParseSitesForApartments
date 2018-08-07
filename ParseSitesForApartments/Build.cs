bef6a294b518a3f39401b09afb2a42b543636087
Posoh <Posoh@POSLAPTOP>
2018-08-07 21:56:37 +0300

    ...

--- ParseSitesForApartments/Build.cs
+++ ParseSitesForApartments/Build.cs
@@ -2,12 +2,14 @@
 {
   public class Build
   {
-    public string Adress { get; set; }
     public string Metro { get; set; }
     public string Distance { get; set; }
     public string CountRoom { get; set; }
     public int Price { get; set; }
     public string Square { get; set; }
     public string Floor { get; set; }
+
+    public string Building { get; set; }
+    public string Street { get; set; }
   }
 }
--- ParseSitesForApartments/Form1.cs
+++ ParseSitesForApartments/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using AngleSharp.Parser.Html;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -25,41 +26,80 @@ namespace ParseSitesForApartments
       using (var webClient = new WebClient())
       {
         Random random = new Random();
-        using (var sw = new StreamWriter(@"D:\Avito1.csv", true, System.Text.Encoding.UTF8))
+        using (var connection = new SqlConnection("Server= localhost; Database= ParseBulding; Integrated Security=True;"))
         {
-          for (int i = pageMin; i < pageMaz; i++)
+          using (var sw = new StreamWriter(@"D:\Avito1.csv", true, System.Text.Encoding.UTF8))
           {
-            Thread.Sleep(random.Next(5000, 10000));
-            string prodam = $@"https://www.avito.ru/sankt-peterburg/kvartiry/prodam?p={i}";
-            webClient.Encoding = System.Text.Encoding.UTF8;
-            var responce = webClient.DownloadString(prodam);
-            var parser = new HtmlParser();
-            var document = parser.Parse(responce);
-
-            var elem = document.GetElementsByClassName("item_table-header");
-            var adresses = document.GetElementsByClassName("address");
-            for (int k = 0; k < elem.Length; k++)
+            for (int i = pageMin; i < pageMaz; i++)
             {
-              var build = new Build();
-              var price = int.Parse(elem[k].GetElementsByClassName("price")[0].TextContent.Trim('\n').Trim('₽').Trim().Replace(" ", ""));
-              build.Price = price;
-              var aboutBuild = elem[k].GetElementsByClassName("item-description-title-link")[0].TextContent.Split(',').ToList();
-              for (int j = 0; j < aboutBuild.Count; j++)
-              {
-                aboutBuild[j] = aboutBuild[j].Trim();
-              }
-              build.CountRoom = aboutBuild[0];
-              build.Square = aboutBuild[1];
-              build.Floor = aboutBuild[2];
+              Thread.Sleep(random.Next(5000, 10000));
+              string prodam = $@"https://www.avito.ru/sankt-peterburg/kvartiry/prodam?p={i}";
+              webClient.Encoding = System.Text.Encoding.UTF8;
+              var responce = webClient.DownloadString(prodam);
+              var parser = new HtmlParser();
+              var document = parser.Parse(responce);
 
-              var adress = adresses[k];
-              build.Metro = adress.ChildNodes[2].NodeValue.Trim();
-              build.Distance = adresses[k].GetElementsByClassName("c-2")[0].TextContent;
-              sw.WriteLine($@"{build.Adress};{build.Price};{build.CountRoom};{build.Square};{build.Floor}");
+              var elem = document.GetElementsByClassName("item_table-header");
+              var adresses = document.GetElementsByClassName("address");
+              for (int k = 0; k < elem.Length; k++)
+              {
+                var build = new Build();
+                var price = int.Parse(elem[k].GetElementsByClassName("price")[0].TextContent.Trim('\n').Trim('₽').Trim().Replace(" ", ""));
+                build.Price = price;
+                var aboutBuild = elem[k].GetElementsByClassName("item-description-title-link")[0].TextContent.Split(',').ToList();
+                for (int j = 0; j < aboutBuild.Count; j++)
+                {
+                  aboutBuild[j] = aboutBuild[j].Trim();
+                }
+                build.CountRoom = aboutBuild[0];
+                build.Square = aboutBuild[1];
+                build.Floor = aboutBuild[2];
+
+                var adress = adresses[k];
+                build.Metro = adress.ChildNodes[2].NodeValue.Trim();
+                build.Distance = adresses[k].GetElementsByClassName("c-2")[0].TextContent;
+
+                var adArr = adress.TextContent.Split(',');
+                build.Street = adArr[adArr.Length - 2].Replace("проспект", "").Replace("пр.", "").Replace("пр-т", "").Replace("ул.", "").Replace("улица", "").Trim();
+                build.Building = adArr[adArr.Length - 1];
+
+                sw.WriteLine($@"{build.Street};{build.Building};{build.Price};{build.CountRoom};{build.Square};{build.Floor}");
+              }
             }
           }
         }
       }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      /*
+      using (var connection = new SqlConnection("Server= localhost; Database= ParseBulding; Integrated Security=True;"))
+      {
+        connection.Open();
+        using (var sr = new StreamReader(@"D:\BuldingData.csv"))
+        {
+          string line;
+          sr.ReadLine();
+          while ((line = sr.ReadLine()) != null)
+          {
+            var arr = line.Split(';');
+            string street = arr[0];
+            string number = arr[1];
+            string bulding = arr[2];
+            string district = arr[3];
+            string buldingDate = arr[4];
+            string repairDate = arr[5];
+
+            string insert = $@"insert into dbo.InfoAboutBulding (District, Street, Number, Bulding, BuldingDate, RepairDate)
+values('{district}','{street}','{number}','{bulding}','{buldingDate}','{repairDate}')";
+
+            var command = new SqlCommand(insert, connection);
+            command.ExecuteNonQuery();
+          }
+        }
+      }
+      */
     }
   }
 }
