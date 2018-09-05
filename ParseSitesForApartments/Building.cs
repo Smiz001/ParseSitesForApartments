f4fde1854acb0c84e5f7f8f02b8006d4ae6f9f2f
PosNik <kenismiz@gmail.com>
2018-09-05 08:29:53 +0300

    create class building

---
+++ ParseSitesForApartments/Building.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseSitesForApartments
+{
+  public class Building
+  {
+    public string Metro { get; set; } = string.Empty;
+    public string Distance { get; set; } = string.Empty;
+    public string Number { get; set; } = string.Empty;
+    public string Structure { get; set; } = string.Empty;
+    public string Liter { get; set; } = string.Empty;
+    public string Street { get; set; } = string.Empty;
+  }
+}
