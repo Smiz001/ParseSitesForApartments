4abd3b70c3319e9d7a37bc18c868c4e8a8561e9a
PosNik <kenismiz@gmail.com>
2019-02-14 10:01:05 +0300

    ...

--- ParseSitesForApartments/Sites/AllSites.cs
+++ ParseSitesForApartments/Sites/AllSites.cs
@@ -71,6 +71,14 @@ namespace ParseSitesForApartments.Sites
       return path;
     }
 
+    private void WaitUntilWorkThread(Thread thread)
+    {
+      while(thread.IsAlive)
+      {
+        Thread.Sleep(10000);
+      }
+    }
+
     public override void ParsingAll()
     {
       string path = ExctractPath();
@@ -79,47 +87,50 @@ namespace ParseSitesForApartments.Sites
       elms.Filename = $@"{path}ELMS.csv";
       elmsThread = new Thread(elms.ParsingAll);
       elmsThread.Start();
+      WaitUntilWorkThread(elmsThread);
       bn.Filename = $@"{path}BN.csv";
       bnThread = new Thread(bn.ParsingAll);
       bnThread.Start();
+      WaitUntilWorkThread(bnThread);
       bkn.Filename = $@"{path}BKN.csv";
       bknThread = new Thread(bkn.ParsingAll);
       bknThread.Start();
+      WaitUntilWorkThread(bknThread);
 
-      //using (var sw = new StreamWriter(new FileStream(Filename, FileMode.Create), Encoding.UTF8))
-      //{
-      //  sw.WriteLine(@"Район;Улица;Номер;Корпус;Литер;Кол-во комнат;Площадь;Этаж;Этажей;Цена;Метро;Дата постройки;Дата реконструкции;Даты кап. ремонты;Общая пл. здания, м2;Жилая пл., м2;Пл. нежелых помещений м2;Мансарда м2;Кол-во проживающих;Центральное отопление;Центральное ГВС;Центральное ЭС;Центарльное ГС;Тип Квартир;Кол-во квартир;Дата ТЭП;Виды кап. ремонта;Общее кол-во лифтов;Расстояние пешком;Время пешком;Расстояние на машине;Время на машине;Откуда взято");
-      //  using (var sr = new StreamReader(elms.Filename))
-      //  {
-      //    sr.ReadLine();
-      //    string line;
-      //    while ((line = sr.ReadLine()) != null)
-      //    {
-      //      sw.WriteLine(line);
-      //    }
-      //  }
-      //  File.Delete(elms.Filename);
-      //  using (var sr = new StreamReader(bn.Filename))
-      //  {
-      //    sr.ReadLine();
-      //    string line;
-      //    while ((line = sr.ReadLine()) != null)
-      //    {
-      //      sw.WriteLine(line);
-      //    }
-      //  }
-      //  File.Delete(bn.Filename);
-      //  using (var sr = new StreamReader(bkn.Filename))
-      //  {
-      //    sr.ReadLine();
-      //    string line;
-      //    while ((line = sr.ReadLine()) != null)
-      //    {
-      //      sw.WriteLine(line);
-      //    }
-      //  }
-      //  File.Delete(bkn.Filename);
-      //}
+      using (var sw = new StreamWriter(new FileStream(Filename, FileMode.Create), Encoding.UTF8))
+      {
+        sw.WriteLine(@"Район;Улица;Номер;Корпус;Литер;Кол-во комнат;Площадь;Этаж;Этажей;Цена;Метро;Дата постройки;Дата реконструкции;Даты кап. ремонты;Общая пл. здания, м2;Жилая пл., м2;Пл. нежелых помещений м2;Мансарда м2;Кол-во проживающих;Центральное отопление;Центральное ГВС;Центральное ЭС;Центарльное ГС;Тип Квартир;Кол-во квартир;Дата ТЭП;Виды кап. ремонта;Общее кол-во лифтов;Расстояние пешком;Время пешком;Расстояние на машине;Время на машине;Откуда взято");
+        using (var sr = new StreamReader(elms.Filename))
+        {
+          sr.ReadLine();
+          string line;
+          while ((line = sr.ReadLine()) != null)
+          {
+            sw.WriteLine(line);
+          }
+        }
+        File.Delete(elms.Filename);
+        using (var sr = new StreamReader(bn.Filename))
+        {
+          sr.ReadLine();
+          string line;
+          while ((line = sr.ReadLine()) != null)
+          {
+            sw.WriteLine(line);
+          }
+        }
+        File.Delete(bn.Filename);
+        using (var sr = new StreamReader(bkn.Filename))
+        {
+          sr.ReadLine();
+          string line;
+          while ((line = sr.ReadLine()) != null)
+          {
+            sw.WriteLine(line);
+          }
+        }
+        File.Delete(bkn.Filename);
+      }
     }
 
     public override void ParsingStudii()
